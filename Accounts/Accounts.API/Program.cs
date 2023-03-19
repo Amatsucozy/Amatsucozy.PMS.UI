f2c3660637e23e34ca52706224cf5501260dbfb2
Huy Truong <huytruong26@outlook.com>
2023-03-19 18:19:39 +0700

    API/UI - Added test authorized api

--- Accounts/Accounts.API/Program.cs
+++ Accounts/Accounts.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://amatsucozy.jp.auth0.com/";
+        options.Audience = "https://localhost:50000/";
+    });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("default", policyBuilder =>
+    {
+        policyBuilder.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +38,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("default");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
